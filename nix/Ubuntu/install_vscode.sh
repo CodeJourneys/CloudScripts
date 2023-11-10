442132ffda1bb33eb2a04cb17f9f7fc7ab81b041
Clark Porter <clarkaporter@outlook.com>
2023-11-10 03:17:34 +0000

    Added nix/Ubuntu/install_vscode.sh

---
+++ nix/Ubuntu/install_vscode.sh
@@ -0,0 +1,15 @@
+# Install vscode on Ubuntu
+# Usage: sudo ./install_vscode.sh
+
+# https://code.visualstudio.com/docs/setup/linux
+apt-get install wget gpg
+# Add the Microsoft GPG key and Microsoft package repository to the system
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+# Install vscode and dependencies
+apt install apt-transport-https
+apt update
+apt install code # or code-insiders
